6e1582d1b0ac331a4227131dba2239b4eab76d3c
sunadoi <y.sunadoi@gmail.com>
2019-12-20 15:50:58 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,4 +5,20 @@ class CardsController < ApplicationController
   def index
     @cards = Card.includes(:user)
   end
+
+  def show
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    # @card.point = params[:card][:point]
+    # @card.save
+    @card.update(card_params)
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:point, :user_id, :admin_id, :relation_id)
+  end
+
 end
