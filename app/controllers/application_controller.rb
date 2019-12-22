446b748d921bc53a677da96d1d678ba0be72b35b
sunadoi <y.sunadoi@gmail.com>
2019-12-22 23:06:22 +0900

    Create before_action :ensure_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
-
-  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def ensure_login
+    unless user_signed_in? || admin_signed_in?
+      redirect_to top_cards_path
+    end
   end
 end
