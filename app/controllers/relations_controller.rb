f18eaef5a22d8cf39dba9db261f0957ee5f33f7b
sunadoi <y.sunadoi@gmail.com>
2019-12-24 17:38:42 +0900

    Update relations_controller.rb

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -1,2 +1,26 @@
 class RelationsController < ApplicationController
+  before_action :set_relation
+
+  def edit
+  end
+
+  def update
+    if @relation.update(relation_params)
+      flash[:notice] = "登録内容を変更しました"
+      redirect_to admin_card_list_index_path(current_admin.id)
+    else
+      flash.now[:alert] = '必要事項を入力してください。'
+      render action: :edit
+    end
+  end
+
+  private
+
+  def relation_params
+    params.permit(:name, :location, :opening_time, :closing_time, :tel, :url)
+  end
+
+  def set_relation
+    @relation = Relation.find(params[:id])
+  end
 end
