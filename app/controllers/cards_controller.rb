90e41bf46f3b091040075e5136205ea71463a69b
sunadoi <y.sunadoi@gmail.com>
2019-12-24 22:53:13 +0900

    Update cards_controller new, create, destroy

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,7 +9,23 @@ class CardsController < ApplicationController
   end
 
   def new
-    @cards = CardList.all
+    @user_cards = Card.includes(:user)
+    @card_lists = CardList.all
+
+    ids = []
+    @user_cards.each do |user_card|
+      ids << user_card.card_list_id
+    end
+    rest_ids = @card_lists.ids - ids
+    @cards = CardList.where(id: rest_ids)
+  end
+
+  def create
+    @card_list = CardList.find(params[:card_list_id])
+    @card = Card.create(card_list_id: @card_list.id, admin_id: @card_list.admin_id, relation_id: @card_list.relation.id, user_id: current_user.id)
+
+    flash[:notice] = "カードを追加しました"
+    redirect_to action: :new
   end
 
   def show
@@ -21,7 +37,15 @@ class CardsController < ApplicationController
     @card.update(card_params)
   end
 
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    flash[:alert] = "カードを削除しました"
+    redirect_to action: :index
+  end
+
   private
+
   def card_params
     params.require(:card).permit(:point, :user_id, :admin_id, :relation_id)
   end
