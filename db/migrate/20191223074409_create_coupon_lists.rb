29130105ac65b2a67045f084aa938e72179c497b
sunadoi <y.sunadoi@gmail.com>
2019-12-24 12:05:45 +0900

    Update migration files

--- db/migrate/20191223074409_create_coupon_lists.rb
+++ db/migrate/20191223074409_create_coupon_lists.rb
@@ -2,7 +2,7 @@ class CreateCouponLists < ActiveRecord::Migration[5.2]
   def change
     create_table :coupon_lists do |t|
       t.references :card_list, foreign_key: true
-      t.text :description, null: false
+      t.text :description
       t.date :expiration
       t.timestamps
     end
