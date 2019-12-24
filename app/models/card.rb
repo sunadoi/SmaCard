25017997f8586509f6488d7d11c286a9a2a6e7bc
sunadoi <y.sunadoi@gmail.com>
2019-12-24 22:53:22 +0900

    Update card.rb

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,7 @@ class Card < ApplicationRecord
   belongs_to :user
   belongs_to :admin
   belongs_to :relation
-  belongs_to :card_list
+  belongs_to :card_list, optional: true
   has_many :cards_benefits
   has_many :benefits, through: :cards_benefits
   has_many :cards_coupons
