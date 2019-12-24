037f7a39f8dc80136b2546514188d22943617c52
sunadoi <y.sunadoi@gmail.com>
2019-12-24 12:07:35 +0900

    Update card_lists_controller.rb

--- app/controllers/card_lists_controller.rb
+++ app/controllers/card_lists_controller.rb
@@ -7,14 +7,20 @@ class CardListsController < ApplicationController
   end
 
   def create
-    @relation = Relation.new(relation_params)
-    @card_list = CardList.new(admin_id: current_admin.id, relation_id: @relation.id)
-    @benefit_list = BenefitList.new(benefit_params)
-    @coupon_list = CouponList.new(coupon_params)
-    if @relation.save && @card_list.save && @benefit_list.save && @coupon_list.save
-      flash[:notice] = "カードを登録しました"
-      redirect_to top_cards_path
-    else
+    begin
+      Relation.transaction do
+        @card_list = CardList.create(admin_id: current_admin.id)
+        @relation = Relation.create!(relation_params)
+        @benefit_list = BenefitList.create(benefit_params)
+        @coupon_list = CouponList.create(coupon_params)
+        flash[:notice] = "カードを登録しました"
+        redirect_to top_cards_path
+      end
+    rescue => e 
+      @card_list = CardList.new(admin_id: current_admin.id)
+      @relation = Relation.new(relation_params)
+      @benefit_list = BenefitList.new(benefit_params)
+      @coupon_list = CouponList.new(coupon_params)
       flash.now[:alert] = '必要事項を入力してください。'
       render action: :new
     end
@@ -23,7 +29,7 @@ class CardListsController < ApplicationController
   private
 
   def relation_params
-    params.require(:relation).permit(:name, :location, :opening_time, :closing_time, :tel, :url)
+    params.require(:relation).permit(:name, :location, :opening_time, :closing_time, :tel, :url).merge(card_list_id: @card_list.id)
   end
 
   def benefit_params
