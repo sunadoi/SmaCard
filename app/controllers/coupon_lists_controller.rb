720ebdc13511375b73e15975eac8821f3b41cdf3
sunadoi <y.sunadoi@gmail.com>
2019-12-24 15:47:54 +0900

    Update coupon_lists_controller

--- app/controllers/coupon_lists_controller.rb
+++ app/controllers/coupon_lists_controller.rb
@@ -1,2 +1,38 @@
 class CouponListsController < ApplicationController
+  before_action :set_coupon, only: [:edit, :update, :destroy]
+
+  def new
+    @coupon_list = CouponList.new
+  end
+
+  def create
+    @coupon_list = CouponList.create(coupon_list_params)
+    flash[:notice] = "特典を追加しました"
+    redirect_to admin_card_list_index_path(current_admin.id)
+  end
+  
+  def edit
+  end
+
+  def update
+    @coupon_list.update(description: params[:description], expiration: params[:expiration])
+    flash[:notice] = "特典情報を変更しました"
+    redirect_to admin_card_list_index_path(current_admin.id)
+  end
+
+  def destroy
+    @coupon_list.destroy
+    flash[:notice] = "特典情報を削除しました"
+    redirect_to admin_card_list_index_path(current_admin.id)
+  end
+
+  private
+
+  def set_coupon
+    @coupon_list = CouponList.find(params[:id].to_i)
+  end
+
+  def coupon_list_params
+    params.permit(:description, :expiration).merge(card_list_id: current_admin.card_list.id)
+  end
 end
