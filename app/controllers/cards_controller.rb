0f959c6580f27e31953203c72274f5ba639dcdb6
sunadoi <y.sunadoi@gmail.com>
2019-12-25 10:36:41 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,7 +9,7 @@ class CardsController < ApplicationController
   end
 
   def new
-    @user_cards = Card.includes(:user)
+    @user_cards = Card.where(user_id: current_user.id)
     @card_lists = CardList.all
 
     ids = []
@@ -18,12 +18,22 @@ class CardsController < ApplicationController
     end
     rest_ids = @card_lists.ids - ids
     @cards = CardList.where(id: rest_ids)
+
+    @benefit_lists = BenefitList.where(card_list_id: @cards.ids)
   end
 
   def create
     @card_list = CardList.find(params[:card_list_id])
     @card = Card.create(card_list_id: @card_list.id, admin_id: @card_list.admin_id, relation_id: @card_list.relation.id, user_id: current_user.id)
 
+    benefit_lists = []
+    @benefit_lists = benefit_params.keys.each do |id|
+      benefit_lists << BenefitList.find(id)
+    end
+    benefit_lists.each do |benefit_list|
+      @benefit = Benefit.create(benefit_list_id: benefit_list.id)
+      @cards_benefits = CardsBenefit.create(card_id: @card.id, benefit_id: @benefit.id)
+    end
     flash[:notice] = "カードを追加しました"
     redirect_to action: :new
   end
@@ -48,6 +58,10 @@ class CardsController < ApplicationController
 
   def card_params
     params.require(:card).permit(:point, :user_id, :admin_id, :relation_id)
+  end
+
+  def benefit_params
+    params.permit(benefit_lists: [:benefit_description])[:benefit_lists]
   end
 
 end
