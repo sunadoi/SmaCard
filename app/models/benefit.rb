08fce3ccf2829fdd55d90a0c69dfa7d0de278d2c
sunadoi <y.sunadoi@gmail.com>
2019-12-25 10:37:11 +0900

    Update models/benefit.rb Add dependent: :delete_all

--- app/models/benefit.rb
+++ app/models/benefit.rb
@@ -1,5 +1,5 @@
 class Benefit < ApplicationRecord
   belongs_to :benefit_list
-  has_many :cards_benefits
+  has_many :cards_benefits, dependent: :delete_all
   has_many :cards, through: :cards_benefits
 end
