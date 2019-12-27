1c5e8abd61f1974423ed7db0556ac5857c80879f
sunadoi <y.sunadoi@gmail.com>
2019-12-27 15:02:15 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
-  
+
+  pass = Faker::Internet.password(8)
+
   factory :user do
-    name {"hoge"}
-    email {Faker::Team.email}
-    password {"hogehoge"}
-    password_confirmation {"hogehoge"}
+    name                  {Faker::Name.name}
+    email                 {Faker::Internet.email}
+    password              {pass}
+    password_confirmation {pass}
     tel {Faker::PhoneNumber.cell_phone}
   end
   
