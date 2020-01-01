665d5edb9b5f25c24af2a8a2c9596ae70406677a
sunadoi <y.sunadoi@gmail.com>
2020-01-01 19:36:27 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_24_103236) do
+ActiveRecord::Schema.define(version: 2020_01_01_095245) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -57,10 +57,8 @@ ActiveRecord::Schema.define(version: 2019_12_24_103236) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "expiration"
-    t.bigint "card_list_id_id"
     t.bigint "card_list_id"
     t.index ["card_list_id"], name: "index_cards_on_card_list_id"
-    t.index ["card_list_id_id"], name: "index_cards_on_card_list_id_id"
   end
 
   create_table "cards_benefits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
